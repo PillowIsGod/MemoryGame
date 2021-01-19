3a2df24329d8a6a66cfb035d7320c31f4f078a27
Eugene Pecherskiy <Chudik.2014@gmail.com>
2021-01-19 12:29:01 +0200

    Memory game complete project kotlin

---
+++ app/src/main/java/utils/Constants.kt
@@ -0,0 +1,19 @@
+package utils
+
+import pillowisgod.com.example.memorygame.R
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_anchor,
+    R.drawable.ic_ball,
+    R.drawable.ic_cloud,
+    R.drawable.ic_dollarsign,
+    R.drawable.ic_drop,
+    R.drawable.ic_face,
+    R.drawable.ic_grass,
+    R.drawable.ic_music,
+    R.drawable.ic_phone,
+    R.drawable.ic_pig,
+    R.drawable.ic_snowflake,
+    R.drawable.ic_sun
+)
